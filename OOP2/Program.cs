ffb11235986d7e0161ff72804d1b2202ecd90d60
halfmoon74 <mustafaselimgil@outlook.com>
2023-07-21 20:55:54 +0300

    Çalışmalar...

---
+++ OOP2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Mustafa Selimgil
+            
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "12345";
+            musteri1.Adi = "Mustafa";
+            musteri1.Soyadi = "Selimgil";
+            musteri1.TcNo = "12345678901";
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "15445";
+            musteri2.SirketAdi = "Kodlama.io";
+            musteri2.VergiNo = "123452222";
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(musteri1);
+            customerManager.Add(musteri2);
+            
+
+        }
+    }
+}
---
+++ OOP3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyaykredimanager = new IhtiyacKrediManager();
+            
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+           
+            IKrediManager konutkredimanager = new KonutKrediManager();
+
+            ILoggerService databaseLoggerservice = new DataBaseLoggerService();
+            
+
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(ihtiyaykredimanager, databaseLoggerservice);
+
+            List<IKrediManager> krediler = new List<IKrediManager>() {ihtiyaykredimanager, tasitKrediManager };
+
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+            
+        }
+    }
+}
